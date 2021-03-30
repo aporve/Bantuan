80d1bab4d858b7286b96d641928f2e96de74482c
abhishek-ym <76812359+abhishek-ym@users.noreply.github.com>
2021-03-30 13:27:59 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,7 +1,17 @@
 document.getElementById('ym-notification').style.bottom = '70px';
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutationRecord) {
+        document.getElementById('ym-notification').style.bottom = '70px';
+    });    
+});
+
+var target = document.getElementById('ym-notification');
+observer.observe(target, { attributes : true, attributeFilter : ['style'] });
 
 
 //testing
+/***
 document.getElementsByClassName("ym-close").addEventListener("click", function() {
     document.getElementById('ym-notification').style.bottom = '70px !important';
 });
+**/
