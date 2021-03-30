fe6d407cc97bb4e8168a99d5c05cd96232a4f7fb
Aporve <aporvep@gmail.com>
2021-03-30 12:34:22 +0530

    testing1

--- custom.js
+++ custom.js
@@ -1,1 +1,10 @@
 document.getElementById('ym-notification').style.bottom = '70px !important';
+
+
+//testing
+
+var test1 = document.getElementById('ymDivCircle')
+
+test1.addEventListener('click', () => {
+    document.getElementById('ym-notification').style.bottom = '70px !important';
+})
