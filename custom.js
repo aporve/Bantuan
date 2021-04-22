ffb6e60059ef5a79406fb06ff1247a3200048e64
abhishek-ym <76812359+abhishek-ym@users.noreply.github.com>
2021-04-22 09:39:47 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
-document.getElementById('ym-notification').style.bottom = '130px';
+document.getElementById('ym-notification').style.bottom = '70px';
 var observer = new MutationObserver(function(mutations) {
-    document.getElementById('ym-notification').style.bottom = '130px'; 
+    document.getElementById('ym-notification').style.bottom = '70px'; 
 });
 var target = document.getElementById('ym-notification');
 observer.observe(target, { attributes : true, attributeFilter : ['style'] });
